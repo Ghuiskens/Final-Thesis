7e452ac34c1437e22468fe21c5c9c440588738bb
Ghuiskens <128136498+Ghuiskens@users.noreply.github.com>
2023-07-14 14:09:11 +0200

    Add files via upload

---
+++ Plus_scraper/Plus_scraper/middlewares.py
@@ -0,0 +1,59 @@
+#Middlwares file for the plus_spider
+#Libraries used in this file
+from scrapy import signals #Handle signals emitted by the spider
+from itemadapter import is_item, ItemAdapter #Adapt item objects
+
+class PlusScraperSpiderMiddleware:
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        #This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
+
+
+class PlusScraperDownloaderMiddleware:
+    @classmethod
+    def from_crawler(cls, crawler):
+        #This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        return None
+
+    def process_response(self, request, response, spider):
+        return response
+
+    def process_exception(self, request, exception, spider):
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
+
+#Create a connection to the the bright data proxy
+class PlusDownloaderMiddleware(object):
+     def process_request(self, request, spider):
+         request.meta['proxy'] = "http://127.0.0.1:24000"
